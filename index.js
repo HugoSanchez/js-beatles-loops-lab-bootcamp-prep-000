c88ff674ccea0e4066539b243c349c354237f1c5
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-06 09:56:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function theBeatlesPlay(musicians, instruments){
+  
+  var array = [];
+  var i = 0
+  
+  for (i = 0; i < musicians.length; i++ ){
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
